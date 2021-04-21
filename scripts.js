41c187c4383e1e01b637cb1f7dcf580c9cc52e52
Bredyn McCombs <d00238943@dmail.dixie.edu>
2021-04-21 00:35:06 -0600

    First commit.

---
+++ scripts.js
@@ -0,0 +1,123 @@
+_currentList = {
+    listName: "Tanks for the memories",
+    units: [
+        {
+            name: "Obi-Won Kenobi",
+            unitCost: 170,
+            models:1,
+            wounds: 6,
+            loyalty: 3,
+            defenseDie: "red",
+            hitSurge: "-",
+            defenseSurge: "-",
+            keywords: {
+                charge: {
+                    description: "after performing a move action, you may attack without spending an action",
+                },
+                jump: {
+                    description: "you may ignore terrain of height [x] or less while performing a move action",
+                    exhausted: false,
+                    isExhaustible: true,
+                    X: 1
+                },
+
+            },
+            weapons: {
+                Lightsaber: {
+                    attackPool: "rrbbww",
+                    range: 0,
+                    keywords: {
+                        critical: {
+                            description: "surge to crit [X] times",
+                            X: 2
+                        },
+                        impact: {
+                            description: "When attacking a unit with armor, change [X] hits to critical hits",
+                            X: 2
+                        },
+                        pierce: {
+                            description: "Ignore [X] blocks from the defending pool",
+                            X: 2,
+                        }
+                    }
+                },
+                saberThrow: {
+                    
+                }
+            },
+            upgrades: {
+                lightSide: {
+                    cap: 2,
+                    upgrades:[
+                        {
+                            name: "Saber Throw",
+                            cost: 5,
+                            action: "add-weapon",
+                            weapon: {
+                                displayText: "Saber Throw",
+                                attackPool: "rbw",
+                                range: {
+                                    start: 1,
+                                    stop: 2
+                                },
+                                keywords: {
+                                    critical: {
+                                        description: "surge to crit [X] times",
+                                        X: 2
+                                    },
+                                    impact: {
+                                        description: "When attacking a unit with armor, change [X] hits to critical hits",
+                                        X: 2
+                                    },
+                                    pierce: {
+                                        description: "Ignore [X] blocks from the defending pool",
+                                        X: 2,
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            name: "Force Reflexes",
+                            cost: 15,
+                            action: "add-keyword",
+                            keyword: {
+                                description: "[exhaust] : add 1 dodge token to this unit",
+                                exhausted: false,
+                                isExhaustible: true
+                            }
+                        }
+                    ]
+                },
+                command: {
+
+                },
+                training: {
+                    
+                }
+            }
+        },
+    ], 
+    orders: {
+        onePip: {
+            generic: {
+                name: "Ambush"
+            }
+
+        },
+        twoPip: {
+            generic: {
+                name: "Push"
+            }
+        },
+        threePip: {
+            generic: {
+                name: "Assault"
+            }
+        },
+        fourPip: {
+            generic: {
+                name: "Standing Orders"    
+            }
+        }
+    }
+}
